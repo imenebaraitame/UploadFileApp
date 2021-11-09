0b99ddee885b89ae9a310296dbda9d3924bd3def
imenebaraitame <baraitame.imene@gmail.com>
2021-11-09 18:42:07 +0100

    initial commit

---
+++ build/resources/main/ratpack.groovy
@@ -0,0 +1,28 @@
+import ratpack.thymeleaf3.ThymeleafModule
+import static ratpack.thymeleaf3.Template.thymeleafTemplate
+
+import static ratpack.groovy.Groovy.ratpack
+
+
+    ratpack {
+        bindings {
+            module(ThymeleafModule)
+
+        }
+        handlers {
+            get{
+                render("Go to upload file page")
+            }
+            get("upload"){
+
+                render (thymeleafTemplate("photo"))
+            }
+            post("ocr"){
+                render(thymeleafTemplate("ocr"))
+            }
+
+        }
+        }
+
+
+
